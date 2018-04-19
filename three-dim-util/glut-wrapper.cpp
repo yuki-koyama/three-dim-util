36a9e089cb2bf9ad821cd424864d6f01f9b05795
Yuki Koyama <yuki@koyama.xyz>
2018-04-19 21:37:17 +0900

    Add glut wrapper functions

---
+++ three-dim-util/glut-wrapper.cpp
@@ -0,0 +1,158 @@
+#include "glut-wrapper.hpp"
+#include <list>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#include <Eigen/Geometry>
+
+namespace threedimutil
+{
+    using Eigen::Vector3d;
+    using Eigen::Matrix4d;
+    
+    inline void glTranslate(const Eigen::Vector3d& t)
+    {
+        glTranslated(t(0), t(1), t(2));
+    }
+    
+    void drawSphere(double r, const Vector3d& p, int res)
+    {
+        glPushMatrix();
+        glTranslate(p);
+        drawSphere(r, res);
+        glPopMatrix();
+    }
+    
+    void drawSphere(double r, int res)
+    {
+        glutSolidSphere(r, res, res);
+    }
+    
+    void drawCylinder(double r, double h, int res, bool center)
+    {
+        drawCylinder(r, r, h, res, center);
+    }
+    
+    void drawCylinder(double r1, double r2, double h, int res, bool center)
+    {
+        glPushMatrix();
+        if (center) glTranslatef(0.0f, 0.0f, - static_cast<GLfloat>(h / 2.0));
+        GLUquadricObj* qobj = gluNewQuadric();
+        gluCylinder(qobj, r1, r2, h, res, 1);
+        glScalef(-1.0f, 1.0f, -1.0f);
+        gluDisk(qobj, 0.0, r1, res, 1);
+        glScalef(-1.0f, 1.0f, -1.0f);
+        glTranslatef(0.0f, 0.0f, static_cast<GLfloat>(h));
+        gluDisk(qobj, 0.0, r2, res, 1);
+        gluDeleteQuadric(qobj);
+        glPopMatrix();
+    }
+    
+    void drawCylinder(double r, const Vector3d& p1, const Vector3d& p2, int res)
+    {
+        Vector3d   t   = p2 - p1;
+        Vector3d   t0  = Vector3d(0.0, 0.0, 1.0);
+        double     c   = t.dot(t0) / t.norm();
+        double     q   = acos(c);
+        Vector3d   ax  = t0.cross(t).normalized();
+        double     h   = t.norm();
+        
+        Eigen::Affine3d rot(Eigen::AngleAxis<double>(q, ax));
+        if (fabs(c - 1.0) < 1e-16) rot = Matrix4d::Identity();
+        
+        glPushMatrix();
+        glTranslate(p1);
+        glMultMatrixd(rot.data()); // rot.data() is column-major
+        drawCylinder(r, h, res, false);
+        glPopMatrix();
+    }
+    
+    void drawCube(double size, bool center)
+    {
+        assert (center);
+        
+        glutSolidCube(size);
+    }
+    
+    void drawCube(double x, double y, double z, bool center)
+    {
+        assert (center);
+        glPushMatrix();
+        glScaled(x, y, z);
+        glutSolidCube(1.0);
+        glPopMatrix();
+    }
+    
+    void drawCube(const Vector3d& translate, double x, double y, double z, bool center)
+    {
+        assert (center);
+        glPushMatrix();
+        glTranslate(translate);
+        glScaled(x, y, z);
+        glutSolidCube(1.0);
+        glPopMatrix();
+    }
+    
+    void drawFrame(double length, double width)
+    {
+        const double l = length;
+        const double m = 0.0;
+        glLineWidth(width);
+        glBegin(GL_LINES);
+        glColor3d (0.9, 0.0, 0.0);
+        glVertex3d(  m, 0.0, 0.0);
+        glVertex3d(  l, 0.0, 0.0);
+        glColor3d (0.0, 0.9, 0.0);
+        glVertex3d(0.0,   m, 0.0);
+        glVertex3d(0.0,   l, 0.0);
+        glColor3d (0.0, 0.0, 0.9);
+        glVertex3d(0.0, 0.0,   m);
+        glVertex3d(0.0, 0.0,   l);
+        glEnd();
+    }
+    
+    void drawRectangle(const Eigen::Vector2d& translate, double x, double y)
+    {
+        drawRectangle(translate(0), translate(1), x, y);
+    }
+    
+    void drawRectangle(double trans_x, double trans_y, double size_x, double size_y)
+    {
+        glMatrixMode(GL_MODELVIEW);
+        glPushMatrix();
+        glTranslated(trans_x, trans_y, 0.0);
+        glBegin(GL_TRIANGLE_FAN);
+        glVertex2d(+ 0.5 * size_x, + 0.5 * size_y);
+        glVertex2d(+ 0.5 * size_x, - 0.5 * size_y);
+        glVertex2d(- 0.5 * size_x, - 0.5 * size_y);
+        glVertex2d(- 0.5 * size_x, + 0.5 * size_y);
+        glEnd();
+        glPopMatrix();
+    }
+    
+    void drawCircle(double r)
+    {
+        const int res = 60;
+        GLUquadricObj* qobj = gluNewQuadric();
+        gluDisk(qobj, 0.0, r, res, 1);
+        gluDeleteQuadric(qobj);
+    }
+    
+    void drawCircle(double r, double trans_x, double trans_y)
+    {
+        glMatrixMode(GL_MODELVIEW);
+        glPushMatrix();
+        glTranslated(trans_x, trans_y, 0.0);
+        drawCircle(r);
+        glPopMatrix();
+    }
+    
+    void drawEllipse(double r_x, double r_y, double trans_x, double trans_y)
+    {
+        glMatrixMode(GL_MODELVIEW);
+        glPushMatrix();
+        glTranslated(trans_x, trans_y, 0.0);
+        glScaled(r_x, r_y, 1.0);
+        drawCircle(1.0);
+        glPopMatrix();
+    }
+}
---
+++ three-dim-util/glut-wrapper.hpp
@@ -0,0 +1,31 @@
+#ifndef GLUT_WRAPPER_HPP
+#define GLUT_WRAPPER_HPP
+
+#include <string>
+#include <vector>
+#include <OpenGL/gl.h>
+#include <Eigen/Core>
+
+namespace threedimutil
+{
+    void drawSphere(double r, const Eigen::Vector3d& p, int res = 60);
+    void drawSphere(double r, int res = 60);
+    void drawCylinder(double r, double h, int res = 60, bool center = true);
+    void drawCylinder(double r1, double r2, double h, int res = 60, bool center = true);
+    void drawCylinder(double r, const Eigen::Vector3d& p1, const Eigen::Vector3d& p2, int res = 60);
+    void drawCube(double size, bool center = true);
+    void drawCube(double x, double y, double z, bool center = true);
+    void drawCube(const Eigen::Vector3d& translate, double x, double y, double z, bool center = true);
+    void drawFrame(double length, double width = 3.0);
+    void drawRectangle(const Eigen::Vector2d& translate, double x, double y);
+    void drawRectangle(double trans_x, double trans_y, double size_x, double size_y);
+    void drawCircle(double r);
+    void drawCircle(double r, double trans_x, double trans_y);
+    void drawEllipse(double r_x, double r_y, double trans_x, double trans_y);
+}
+
+inline void glColor(const Eigen::Vector3d& c) { glColor3d(c(0), c(1), c(2)); }
+inline void glColor(const Eigen::Vector3d& c, double a) { glColor4d(c(0), c(1), c(2), a); }
+inline void glVertex(const Eigen::Vector2d& v) { glVertex2d(v(0), v(1)); }
+
+#endif // GLUT_WRAPPER_HPP
