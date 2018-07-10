85df0ccb1cc7a6a65de06c2982e0c92d55c6ac4a
Yuki Koyama <yuki@koyama.xyz>
2018-07-10 11:06:33 +0900

    Add makeOrtho2d function

--- three-dim-util/matrix.hpp
+++ three-dim-util/matrix.hpp
@@ -23,6 +23,21 @@ namespace threedimutil
                                           0.0,      0.0, - 2.0 / dz,  tz,
                                           0.0,      0.0,        0.0, 1.0).finished();
     }
+    
+    inline Eigen::Matrix4d makeOrtho2d(double left,
+                                       double right,
+                                       double bottom,
+                                       double top)
+    {
+        const double dx = right - left;
+        const double dy = top - bottom;
+        const double tx = - (right + left) / dx;
+        const double ty = - (top + bottom) / dy;
+        return (Eigen::Matrix4d() << 2.0 / dx,      0.0,   0.0,  tx,
+                                          0.0, 2.0 / dy,   0.0,  ty,
+                                          0.0,      0.0, - 1.0, 0.0,
+                                          0.0,      0.0,   0.0, 1.0).finished();
+    }
 }
 
 #endif /* matrix_hpp */
