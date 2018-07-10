e6f61ae9777cb450927593f9641b041bf9b1f449
Yuki Koyama <yuki@koyama.xyz>
2018-07-10 10:59:43 +0900

    Add a file for implementing matrix-related helper functions

---
+++ three-dim-util/matrix.hpp
@@ -0,0 +1,28 @@
+#ifndef matrix_hpp
+#define matrix_hpp
+
+#include <Eigen/Core>
+
+namespace threedimutil
+{
+    inline Eigen::Matrix4d makeOrtho(double left,
+                                     double right,
+                                     double bottom,
+                                     double top,
+                                     double near,
+                                     double far)
+    {
+        const double dx = right - left;
+        const double dy = top - bottom;
+        const double dz = far - near;
+        const double tx = - (right + left) / dx;
+        const double ty = - (top + bottom) / dy;
+        const double tz = - (far + near) / dz;
+        return (Eigen::Matrix4d() << 2.0 / dx,      0.0,        0.0,  tx,
+                                          0.0, 2.0 / dy,        0.0,  ty,
+                                          0.0,      0.0, - 2.0 / dz,  tz,
+                                          0.0,      0.0,        0.0, 1.0).finished();
+    }
+}
+
+#endif /* matrix_hpp */
