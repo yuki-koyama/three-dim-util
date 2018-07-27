ccbc8acabbef6c1096a9f8970847e28066ae1de5
Yuki Koyama <yuki@koyama.xyz>
2018-07-27 19:42:37 +0900

    Add draw utility functions

---
+++ three-dim-util/draw-functions.hpp
@@ -0,0 +1,37 @@
+#ifndef draw_utils_hpp
+#define draw_utils_hpp
+
+#include <Eigen/Core>
+#include <three-dim-util/gl.hpp>
+
+namespace threedimutil
+{
+    inline void draw_edges(const Eigen::MatrixXd& P, const Eigen::MatrixXi& E)
+    {
+        assert(P.cols() == 3);
+        assert(E.cols() == 2);
+        glBegin(GL_LINES);
+        for (int i = 0; i < E.rows(); ++ i)
+        {
+            threedimutil::vertex_3d(P.row(E(i, 0)));
+            threedimutil::vertex_3d(P.row(E(i, 1)));
+        }
+        glEnd();
+    }
+    
+    inline void draw_faces(const Eigen::MatrixXd& V, const Eigen::MatrixXi& F)
+    {
+        assert(V.cols() == 3);
+        assert(F.cols() == 3);
+        glBegin(GL_TRIANGLES);
+        for (int i = 0; i < F.rows(); ++ i)
+        {
+            threedimutil::vertex_3d(V.row(F(i, 0)));
+            threedimutil::vertex_3d(V.row(F(i, 1)));
+            threedimutil::vertex_3d(V.row(F(i, 2)));
+        }
+        glEnd();
+    }
+}
+
+#endif /* draw_utils_hpp */
