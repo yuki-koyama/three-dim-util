8191bb3a4d02c7f0ddca9fd71c1baa51315545a7
Yuki Koyama <yuki@koyama.xyz>
2018-08-13 12:33:20 +0900

    Add demo codes

--- demos/trackball-widget-demo/main.cpp
+++ demos/trackball-widget-demo/main.cpp
@@ -1,4 +1,34 @@
-int main()
+#include <three-dim-util/trackball-widget.hpp>
+#include <three-dim-util/draw-functions.hpp>
+#include <Eigen/Core>
+#include <QApplication>
+
+class Widget : public threedimutil::TrackballWidget
 {
-    return 0;
+public:
+    Widget(QWidget *parent = nullptr) : threedimutil::TrackballWidget(parent) {}
+    
+protected:
+    void paintGL()
+    {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        
+        setProjectionMatrix();
+        setModelViewMatrix();
+        
+        glPointSize(8.0);
+        threedimutil::draw_points(points_, colors_);
+    }
+    
+private:
+    const Eigen::MatrixXd points_ = Eigen::MatrixXd::Random(200, 3);
+    const Eigen::MatrixXd colors_ = 0.5 * (points_ + Eigen::MatrixXd::Ones(200, 3));
+};
+
+int main(int argc, char** argv)
+{
+    QApplication app(argc, argv);
+    Widget widget;
+    widget.show();
+    return app.exec();
 }
