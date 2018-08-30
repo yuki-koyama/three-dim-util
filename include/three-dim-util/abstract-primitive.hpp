9dd519528e169efeacbb2f278070b93a0a9909e2
Yuki Koyama <yuki@koyama.xyz>
2018-08-30 21:58:11 +0900

    Use an abstract class for defining each draw primitive

---
+++ include/three-dim-util/abstract-primitive.hpp
@@ -0,0 +1,87 @@
+#ifndef abstract_primitive_hpp
+#define abstract_primitive_hpp
+
+#include <three-dim-util/gl.hpp>
+#include <iostream>
+#include <Eigen/Core>
+
+namespace threedimutil
+{
+    class AbstractPrimitive
+    {
+    public:
+        // static void Initialize();
+        // static void Draw();
+        // static AbstractPrimitive& GetInstance();
+        
+    protected:
+        void InitializeInternal()
+        {
+            if (ready_)
+            {
+                std::cerr << "Warning: Initialize() is called but it is already initialized. This call is ignored." << std::endl;
+                return;
+            }
+            
+            CreateVertexData();
+            
+            glGenBuffers(1, &vertex_vbo_);
+            glBindBuffer(GL_ARRAY_BUFFER, vertex_vbo_);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(GLdouble) * vertices_.size(), vertices_.data(), GL_STATIC_DRAW);
+            
+            glGenBuffers(1, &normal_vbo_);
+            glBindBuffer(GL_ARRAY_BUFFER, normal_vbo_);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(GLdouble) * normals_.size(), normals_.data(), GL_STATIC_DRAW);
+            
+            ready_ = true;
+        }
+        
+        void DrawInternal()
+        {
+            if (!ready_) { InitializeInternal(); }
+            
+            glEnableClientState(GL_VERTEX_ARRAY);
+            glEnableClientState(GL_NORMAL_ARRAY);
+            
+            glBindBuffer(GL_ARRAY_BUFFER, vertex_vbo_);
+            glVertexPointer(3, GL_DOUBLE, 0, NULL);
+            
+            glBindBuffer(GL_ARRAY_BUFFER, normal_vbo_);
+            glNormalPointer(GL_DOUBLE, 0, NULL);
+            
+            glDrawArrays(GL_TRIANGLES, 0, vertices_.cols());
+            
+            glBindBuffer(GL_ARRAY_BUFFER, 0);
+            
+            glDisableClientState(GL_VERTEX_ARRAY);
+            glDisableClientState(GL_NORMAL_ARRAY);
+        }
+        
+        AbstractPrimitive() = default;
+        ~AbstractPrimitive()
+        {
+            if (ready_)
+            {
+                glDeleteBuffers(1, &vertex_vbo_);
+                glDeleteBuffers(1, &normal_vbo_);
+            }
+        }
+        AbstractPrimitive(const AbstractPrimitive&)            = delete;
+        AbstractPrimitive& operator=(const AbstractPrimitive&) = delete;
+        AbstractPrimitive(AbstractPrimitive&&)                 = delete;
+        AbstractPrimitive& operator=(AbstractPrimitive&&)      = delete;
+        
+        Eigen::MatrixXd vertices_;
+        Eigen::MatrixXd normals_;
+        
+    private:
+        virtual void CreateVertexData() = 0;
+        
+        bool ready_ = false;
+        
+        GLuint vertex_vbo_;
+        GLuint normal_vbo_;
+    };
+}
+
+#endif /* abstract_primitive_hpp */
--- include/three-dim-util/sphere.hpp
+++ include/three-dim-util/sphere.hpp
@@ -1,18 +1,19 @@
 #ifndef sphere_hpp
 #define sphere_hpp
 
-#include <three-dim-util/gl.hpp>
-#include <iostream>
+#include <three-dim-util/abstract-primitive.hpp>
 #include <Eigen/Core>
 
 namespace threedimutil
 {
-    class Sphere
+    class Sphere : public AbstractPrimitive
     {
     public:
         static void Initialize(int latitude_resolution = 20, int longitude_resolution = 30)
         {
-            Sphere::GetInstance().InitializeInternal(latitude_resolution, longitude_resolution);
+            Sphere::GetInstance().latitude_resolution()  = latitude_resolution;
+            Sphere::GetInstance().longitude_resolution() = longitude_resolution;
+            Sphere::GetInstance().InitializeInternal();
         }
         
         static void Draw()
@@ -20,106 +21,54 @@ namespace threedimutil
             Sphere::GetInstance().DrawInternal();
         }
         
-    protected:
         static Sphere& GetInstance()
         {
             static Sphere sphere;
             return sphere;
         }
         
-        void InitializeInternal(int latitude_resolution = 20, int longitude_resolution = 30)
+        int latitude_resolution() const { return latitude_resolution_; }
+        int& latitude_resolution() { return latitude_resolution_; }
+        int longitude_resolution() const { return longitude_resolution_; }
+        int& longitude_resolution() { return longitude_resolution_; }
+        
+    private:
+        void CreateVertexData()
         {
             constexpr double pi = M_PI;
             
-            if (ready_)
+            vertices_.resize(3, latitude_resolution_ * longitude_resolution_ * 6);
+            for (int i = 0; i < longitude_resolution_; ++ i)
             {
-                std::cerr << "Warning: Initialize() is called but it is already initialized. This call is ignored." << std::endl;
-                return;
-            }
-            
-            vertices_.resize(3, latitude_resolution * longitude_resolution * 6);
-            for (int i = 0; i < longitude_resolution; ++ i)
-            {
-                const double theta_xy_1 = 2.0 * static_cast<double>(i + 0) * pi / static_cast<double>(longitude_resolution);
-                const double theta_xy_2 = 2.0 * static_cast<double>(i + 1) * pi / static_cast<double>(longitude_resolution);
+                const double theta_xy_1 = 2.0 * static_cast<double>(i + 0) * pi / static_cast<double>(longitude_resolution_);
+                const double theta_xy_2 = 2.0 * static_cast<double>(i + 1) * pi / static_cast<double>(longitude_resolution_);
                 const double x_1        = std::cos(theta_xy_1);
                 const double x_2        = std::cos(theta_xy_2);
                 const double y_1        = std::sin(theta_xy_1);
                 const double y_2        = std::sin(theta_xy_2);
                 
-                for (int j = 0; j < latitude_resolution; ++ j)
+                for (int j = 0; j < latitude_resolution_; ++ j)
                 {
-                    const double theta_z_1 = static_cast<double>(j + 0) * pi / static_cast<double>(latitude_resolution);
-                    const double theta_z_2 = static_cast<double>(j + 1) * pi / static_cast<double>(latitude_resolution);
+                    const double theta_z_1 = static_cast<double>(j + 0) * pi / static_cast<double>(latitude_resolution_);
+                    const double theta_z_2 = static_cast<double>(j + 1) * pi / static_cast<double>(latitude_resolution_);
                     const double cos_1 = std::cos(theta_z_1);
                     const double cos_2 = std::cos(theta_z_2);
                     const double sin_1 = std::sin(theta_z_1);
                     const double sin_2 = std::sin(theta_z_2);
                     
-                    vertices_.col(i * latitude_resolution * 6 + j * 6 + 0) = Eigen::Vector3d(sin_2 * x_1, sin_2 * y_1, cos_2);
-                    vertices_.col(i * latitude_resolution * 6 + j * 6 + 1) = Eigen::Vector3d(sin_2 * x_2, sin_2 * y_2, cos_2);
-                    vertices_.col(i * latitude_resolution * 6 + j * 6 + 2) = Eigen::Vector3d(sin_1 * x_2, sin_1 * y_2, cos_1);
-                    vertices_.col(i * latitude_resolution * 6 + j * 6 + 3) = Eigen::Vector3d(sin_2 * x_1, sin_2 * y_1, cos_2);
-                    vertices_.col(i * latitude_resolution * 6 + j * 6 + 4) = Eigen::Vector3d(sin_1 * x_2, sin_1 * y_2, cos_1);
-                    vertices_.col(i * latitude_resolution * 6 + j * 6 + 5) = Eigen::Vector3d(sin_1 * x_1, sin_1 * y_1, cos_1);
+                    vertices_.col(i * latitude_resolution_ * 6 + j * 6 + 0) = Eigen::Vector3d(sin_2 * x_1, sin_2 * y_1, cos_2);
+                    vertices_.col(i * latitude_resolution_ * 6 + j * 6 + 1) = Eigen::Vector3d(sin_2 * x_2, sin_2 * y_2, cos_2);
+                    vertices_.col(i * latitude_resolution_ * 6 + j * 6 + 2) = Eigen::Vector3d(sin_1 * x_2, sin_1 * y_2, cos_1);
+                    vertices_.col(i * latitude_resolution_ * 6 + j * 6 + 3) = Eigen::Vector3d(sin_2 * x_1, sin_2 * y_1, cos_2);
+                    vertices_.col(i * latitude_resolution_ * 6 + j * 6 + 4) = Eigen::Vector3d(sin_1 * x_2, sin_1 * y_2, cos_1);
+                    vertices_.col(i * latitude_resolution_ * 6 + j * 6 + 5) = Eigen::Vector3d(sin_1 * x_1, sin_1 * y_1, cos_1);
                 }
             }
             normals_ = vertices_;
-            
-            glGenBuffers(1, &vertex_vbo_);
-            glBindBuffer(GL_ARRAY_BUFFER, vertex_vbo_);
-            glBufferData(GL_ARRAY_BUFFER, sizeof(GLdouble) * vertices_.size(), vertices_.data(), GL_STATIC_DRAW);
-            
-            glGenBuffers(1, &normal_vbo_);
-            glBindBuffer(GL_ARRAY_BUFFER, normal_vbo_);
-            glBufferData(GL_ARRAY_BUFFER, sizeof(GLdouble) * normals_.size(), normals_.data(), GL_STATIC_DRAW);
-            
-            ready_ = true;
         }
         
-        void DrawInternal()
-        {
-            if (!ready_) { Initialize(); }
-            
-            glEnableClientState(GL_VERTEX_ARRAY);
-            glEnableClientState(GL_NORMAL_ARRAY);
-            
-            glBindBuffer(GL_ARRAY_BUFFER, vertex_vbo_);
-            glVertexPointer(3, GL_DOUBLE, 0, NULL);
-            
-            glBindBuffer(GL_ARRAY_BUFFER, normal_vbo_);
-            glNormalPointer(GL_DOUBLE, 0, NULL);
-            
-            glDrawArrays(GL_TRIANGLES, 0, vertices_.cols());
-            
-            glBindBuffer(GL_ARRAY_BUFFER, 0);
-            
-            glDisableClientState(GL_VERTEX_ARRAY);
-            glDisableClientState(GL_NORMAL_ARRAY);
-        }
-        
-    private:
-        Sphere() = default;
-        ~Sphere()
-        {
-            if (ready_)
-            {
-                glDeleteBuffers(1, &vertex_vbo_);
-                glDeleteBuffers(1, &normal_vbo_);
-            }
-        }
-        Sphere(const Sphere&)            = delete;
-        Sphere& operator=(const Sphere&) = delete;
-        Sphere(Sphere&&)                 = delete;
-        Sphere& operator=(Sphere&&)      = delete;
-        
-        bool ready_ = false;
-        
-        GLuint vertex_vbo_;
-        GLuint normal_vbo_;
-        
-        Eigen::MatrixXd vertices_;
-        Eigen::MatrixXd normals_;
+        int latitude_resolution_  = 20;
+        int longitude_resolution_ = 30;
     };
 }
 
