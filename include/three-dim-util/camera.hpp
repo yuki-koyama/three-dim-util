7ab4127d6d2d2a6a44c9820926d25c0e1ccad94f
Yuki Koyama <yuki@koyama.xyz>
2019-08-20 19:46:48 +0900

    Add lookat method to the camera class and fix coding style

--- include/three-dim-util/camera.hpp
+++ include/three-dim-util/camera.hpp
@@ -10,7 +10,7 @@ namespace threedimutil
     {
     public:
         Camera();
-        
+
         enum class Mode
         {
             Rotate,
@@ -18,34 +18,37 @@ namespace threedimutil
             Zoom,
             None
         };
-        
-        double& vertical_angle_of_view() { return vertical_angle_of_view_; }
-        double vertical_angle_of_view() const { return vertical_angle_of_view_; }
-        
-        Eigen::Vector3d& position() { return position_; }
-        const Eigen::Vector3d& position() const { return position_; }
-        Eigen::Vector3d& target() { return target_; }
-        const Eigen::Vector3d& target() const { return target_; }
-        Eigen::Vector3d& up() { return up_; }
-        const Eigen::Vector3d& up() const { return up_; }
-        
+
+        double& vertical_angle_of_view() { return m_vertical_angle_of_view; }
+        double vertical_angle_of_view() const { return m_vertical_angle_of_view; }
+
+        Eigen::Vector3d& position() { return m_position; }
+        const Eigen::Vector3d& position() const { return m_position; }
+        Eigen::Vector3d& target() { return m_target; }
+        const Eigen::Vector3d& target() const { return m_target; }
+        Eigen::Vector3d& up() { return m_up; }
+        const Eigen::Vector3d& up() const { return m_up; }
+
+        // Method for obtaining matrices
+        Eigen::Matrix4d GetLookAtMatrix() const;
+
         // Method for animated visualization
         void RotateAroundTarget(double theta_in_radian);
-        
+
         // Methods for cursor interaction
         void BeginTrackball(int x, int y, Mode mode);
-        void MoveTrackball (int x, int y);
-        void EndTrackball  ();
-        
+        void MoveTrackball(int x, int y);
+        void EndTrackball();
+
     private:
-        double vertical_angle_of_view_ = M_PI * 45.0 / 180.0;
-        
-        Eigen::Vector3d position_;
-        Eigen::Vector3d target_;
-        Eigen::Vector3d up_;
-        
-        Mode mode_;
-        Eigen::Vector2i prev_position_;
+        double m_vertical_angle_of_view = M_PI * 45.0 / 180.0;
+
+        Eigen::Vector3d m_position;
+        Eigen::Vector3d m_target;
+        Eigen::Vector3d m_up;
+
+        Mode m_mode;
+        Eigen::Vector2i m_prev_position;
     };
 }
 
--- src/camera.cpp
+++ src/camera.cpp
@@ -4,93 +4,111 @@
 namespace threedimutil
 {
     using namespace Eigen;
-    
+
     Camera::Camera() :
-    position_(- 2.0, 0.0, 3.0),
-    target_(0.0, 0.0, 0.0),
-    up_(0.0, 1.0, 0.0),
-    mode_(Mode::None)
+    m_position(- 2.0, 0.0, 3.0),
+    m_target(0.0, 0.0, 0.0),
+    m_up(0.0, 1.0, 0.0),
+    m_mode(Mode::None)
     {
     }
-    
+
     void Camera::RotateAroundTarget(double theta_in_radian)
     {
         // Horizontal rotation
-        const Matrix3d rot_h = AngleAxisd(theta_in_radian, up_).matrix();
-        
+        const Matrix3d rot_h = AngleAxisd(theta_in_radian, m_up).matrix();
+
         // Update the camera position
-        position_ = target_ + rot_h * (position_ - target_);
+        m_position = m_target + rot_h * (m_position - m_target);
     }
-    
+
     void Camera::BeginTrackball(int x, int y, Mode mode)
     {
-        mode_ = mode;
-        prev_position_ = Vector2i(x, y);
+        m_mode = mode;
+        m_prev_position = Vector2i(x, y);
     }
-    
+
     void Camera::MoveTrackball(int x, int y)
     {
         constexpr double scale_x = 2000.0;
         constexpr double scale_y = 2000.0;
-        
-        switch (mode_)
+
+        switch (m_mode)
         {
             case Mode::None:
                 break;
             case Mode::Pan:
             {
-                const Vector3d eye      = position_ - target_;
-                const Vector3d left_dir = eye.cross(up_);
+                const Vector3d eye      = m_position - m_target;
+                const Vector3d left_dir = eye.cross(m_up);
                 const double   len      = eye.norm();
-                const double   diff_x   = static_cast<double>(x - prev_position_(0)) / scale_x;
-                const double   diff_y   = static_cast<double>(y - prev_position_(1)) / scale_y;
+                const double   diff_x   = static_cast<double>(x - m_prev_position(0)) / scale_x;
+                const double   diff_y   = static_cast<double>(y - m_prev_position(1)) / scale_y;
                 const Vector3d trans_x  = len * diff_x * left_dir.normalized();
-                const Vector3d trans_y  = len * diff_y * up_.normalized();
-                position_ += trans_x;
-                position_ += trans_y;
-                target_   += trans_x;
-                target_   += trans_y;
+                const Vector3d trans_y  = len * diff_y * m_up.normalized();
+                m_position += trans_x;
+                m_position += trans_y;
+                m_target   += trans_x;
+                m_target   += trans_y;
                 break;
             }
             case Mode::Rotate:
             {
-                const double theta_x = (2.0 * M_PI * static_cast<double>(x - prev_position_(0))) / scale_x;
-                const double theta_y = (2.0 * M_PI * static_cast<double>(y - prev_position_(1))) / scale_y;
-                
+                const double theta_x = (2.0 * M_PI * static_cast<double>(x - m_prev_position(0))) / scale_x;
+                const double theta_y = (2.0 * M_PI * static_cast<double>(y - m_prev_position(1))) / scale_y;
+
                 // Horizontal rotation
-                const Matrix3d rot_h = AngleAxisd(- theta_x, up_).matrix();
-                Vector3d eye = position_ - target_;
-                
+                const Matrix3d rot_h = AngleAxisd(- theta_x, m_up).matrix();
+                Vector3d eye = m_position - m_target;
+
                 // Vertical rotation
-                const Vector3d left_dir = eye.cross(up_).normalized();
+                const Vector3d left_dir = eye.cross(m_up).normalized();
                 const Matrix3d rot_v    = AngleAxisd(theta_y, left_dir).matrix();
-                
-                const double test = left_dir.dot((rot_v * eye).cross(up_));
+
+                const double test = left_dir.dot((rot_v * eye).cross(m_up));
                 if (test > 0.0) {
                     eye = rot_v * rot_h * eye;
                 }
-                
-                position_ = target_ + eye;
+
+                m_position = m_target + eye;
                 break;
             }
             case Mode::Zoom:
             {
                 constexpr double scale = 0.5;
-                
-                const double   speed   = static_cast<double>(y - prev_position_(1)) / scale_y;
-                const Vector3d eye_ray = speed * (target_ - position_);
-                position_ += scale * eye_ray;
+
+                const double   speed   = static_cast<double>(y - m_prev_position(1)) / scale_y;
+                const Vector3d eye_ray = speed * (m_target - m_position);
+                m_position += scale * eye_ray;
                 break;
             }
             default:
                 break;
         }
-        
-        prev_position_ = Vector2i(x, y);
+
+        m_prev_position = Vector2i(x, y);
     }
-    
+
     void Camera::EndTrackball()
     {
-        mode_ = Mode::None;
+        m_mode = Mode::None;
     }
+}
+
+Eigen::Matrix4d threedimutil::Camera::GetLookAtMatrix() const
+{
+    Eigen::Matrix4d mat = Eigen::Matrix4d::Identity();
+
+    const Eigen::Vector3d forward = (m_target - m_position).normalized();
+    const Eigen::Vector3d side    = forward.cross(m_up);
+    const Eigen::Vector3d up      = side.cross(forward);
+
+    mat.block<1, 3>(0, 0) = side.transpose();
+    mat.block<1, 3>(1, 0) = up.transpose();
+    mat.block<1, 3>(2, 0) = - forward.transpose();
+    mat(0, 3) = - side.dot(m_position);
+    mat(1, 3) = - up.dot(m_position);
+    mat(2, 3) = forward.dot(m_position);
+
+    return mat;
 }
